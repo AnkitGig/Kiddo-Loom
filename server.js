45a0a66d6795f4d606c6d2a0c31791ec6908387e
anand-patwa-nenosystems <146060523+anand-patwa-nenosystems@users.noreply.github.com>
2025-10-25 17:26:56 +0530

    Add a HTML Pages faq, aboutus, help&support, privacyPolicy and  terms Page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import  "dotenv/config.js"
 import {connectDB} from './config/db.js';
+import rootRouter from './routes/root.routes.js';
 import morgan from 'morgan';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -24,7 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
+app.use(express.static(path.join(__dirname, "view")));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'view')); 
@@ -34,7 +35,6 @@ app.use("/api/v1",express.static("public"))
 
 
 // ✅ Routes
-import rootRouter from './routes/root.routes.js';
 app.use("/api/v1", rootRouter);
 
 
@@ -42,7 +42,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "about.html"));
+});
+app.get("/terms", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "terms.html"));
+});
+app.get("/privacyPolicy", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "privacyPolicy.html"));
+});
+app.get("/faq", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "faq.html"));
+});
+app.get("/help&support", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "help&support.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
