62f2b54ae70ef455c12817799bdcfc266353da27
anand-patwa-nenosystems <146060523+anand-patwa-nenosystems@users.noreply.github.com>
2025-11-13 19:08:40 +0530

    Minnor Bug Fix Uload Routes are Missing

--- routes/root.routes.js
+++ routes/root.routes.js
@@ -5,7 +5,6 @@ import superAdminRouter from "./superAdmin.routes.js";
 import adminRouter from "./admin.routes.js";
 import teacherRouter from "./teacher.routes.js";
 import dailyReportRouter from "./dailyReport.routes.js";
-import uploadRouter from "./upload.routes.js";
 
 
 
@@ -17,7 +16,6 @@ rootRouter.use("/admin", adminRouter)
 rootRouter.use("/super-admin", superAdminRouter);
 rootRouter.use("/teachers", teacherRouter);
 rootRouter.use("/daily-reports", dailyReportRouter);
-rootRouter.use("/upload", uploadRouter);
 
 
 
